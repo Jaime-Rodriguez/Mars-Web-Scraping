0b0d27d23c45bba5a2029156d615d8308d3a58a6
Jaime Rodriguez <rodriguez.jaime.a@gmail.com>
2019-08-21 21:49:11 -0700

    Finished work on index.html and ready to submit

--- app.py
+++ app.py
@@ -1,26 +1,22 @@
 from flask import Flask, render_template, redirect
-from flask_pymongo import PyMongo
+import pymongo
 import scrape_mars
 
 # Create an instance of our Flask app.
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri="mongodb://localhost:27017/mission_to_mars")
-
-# Connect to a database. Will create one if not already available.
-# db = client.mars_db
-# # Drops collection if available to remove duplicates; or creates a new one
-# db.mission_to_mars.drop()
-# Import the dictionary in the database
+conn = 'mongodb://localhost:27017'
+client = pymongo.MongoClient(conn)
+db=client.mission_to_mars
 
 @app.route('/scrape/')
 def scrape():
-   #Call the scrape function from the scrape_mars.py file
+   # Run the scrape function from scrape_mars.py 
    result = scrape_mars.scrape()
-   #Store/Write the result disctionary into MongoDB
-   # db.mission_to_mars.update(result)
-   mongo.db.mission_to_mars.update({}, result, upsert=True)
+   # Load the dictionary to the database
+   db.mission_to_mars.update({}, result, upsert=True)
+   # Return to the homepage
    return redirect('/')
 
 # Set route
@@ -28,7 +24,9 @@ def scrape():
 @app.route('/')
 def index():
    # Store the entire mars collection in a list
-   mars_list = mongo.db.mission_to_mars.find_one()
+   mars_list = db.mission_to_mars.find_one()
+ 
+   # mars_list = mongo.db.mission_to_mars.find_one()
    print(mars_list)
    # Return the template with the mars_list passed in
    return render_template('index.html', mars_list=mars_list)
